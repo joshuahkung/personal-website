229d234a0d9f6438fe549482a930a6d2026c8c92
Josh <jhkung1@gmail.com>
2026-02-17 19:55:25 -0500

    Blog: Learnings

--- app/blog/posts/myLearnings.tsx
+++ app/blog/posts/myLearnings.tsx
@@ -7,11 +7,11 @@ export const Learnings = () => {
         {`A short, unordered collection of important learnings acrewed over the years:
 
         - I am here
-        - Be present 
+        - Be present (Eric Sohn)
         - Be mindful of your inner dialogue, make it positive 
         - Be humble 
-        - Take care of your body and mind, you only have one
-        - How you do anything is how you do everything
+        - Take care of your body and mind, you only have one (my dad)
+        - How you do anything is how you do everything (my mom)
         - Food should always taste amazing
         - Food tastes best when shared 
         - Listen to lots of new music and try new foods 
@@ -19,11 +19,11 @@ export const Learnings = () => {
         - Winter is the best season
         - Do exactly what you want
         - Time is your most important resource, try not to waste it
-        - Life can change for the better in just one day or even one moment so stay viligant
+        - You just need one good day to turn your life around (William Hwang)
         - Be the brother, friend, son you wish you had
         - What you say can build or break people 
         - It is easier to shape the future than to predict it
-        - The Law of Equivalent Exchange: to gain something, one must first give up something of equal value
+        - The Law of Equivalent Exchange: to gain something, one must first give up something of equal value (FMAB)
         - Slow motion is better than no motion
         - You can only control your thoughts and actions, everything else is beyond your control 
         - If you can't control yourself, how can you expect to control outcomes
@@ -34,6 +34,10 @@ export const Learnings = () => {
         - Be happy
         - Be kind
         - Your successes and failures are often influenced by circumstance; this should make you more humble and compassionate towards others
+        - Don't prioritize your schedule, schedule your priorities (Aditya Guruprasad)
+        - Why save [it] for a better occasion when you can make this occasion better? (Danny Wu)
+
+        Last edited 02/17/2026
 
         `}
       </p>
